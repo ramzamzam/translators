4cfb03f0072fb67beb88ba633cacc11c0a7f7c68
Yevheniy Miropolets <yevheniy.miropolets@gmail.com>
2017-05-13 07:20:50 +0300

    add client

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+const SA = require('./lib/syntatic_analyzer');
+const RPNBuilder = require('./lib/poliz');
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
@@ -13,11 +16,33 @@ app.get('/', function (req, res) {
   res.sendFile('public/index.html');
 });
 
+let _tables;
 app.post('/translate', function(req, res) {
 	// console.log(req)
-	console.log(req.body.code);
-	return res.json({done : true});
+	try {
+        const tables = SA(req.body.code);
+        _tables = tables;
+        return res.status(200).json(tables);
+    } catch(err) {
+	    console.log(err)
+	    res.status(500).end(err.toString().replace('\n', '¶'));
+    }
+	// return res.json({done : true});
 });
+
+app.post('/rpn', function (req, res) {
+    const builder = new RPNBuilder(_tables);
+    try {
+        const result = builder.extractLabels(builder.build());
+        result.history = builder.rpnHistory;
+        console.log({history : result.history});
+        return res.json(result);
+
+    } catch (err) {
+        console.log(err);
+        return res.status(500).end(err.toString())
+    }
+})
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,14 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
-
+let lexTables;
+document.addEventListener("DOMContentLoaded", function(event) {
+    // const editor = ace.edit('editor');
+    // editor.getSession().setMode("ace/mode/javascript");
 	const codeArea = document.querySelector('textarea#code');
+    // $('textarea#code').numberedtextarea({
+	 //    color : '#000'
+    // });
+	if(localStorage['code-translators']) codeArea.value = localStorage['code-translators'];
 	const translateCode = document.querySelector('a#js-translate-code') ;
-
+    const buildRPNButton = document.querySelector('a#js-build-rpn');
 	translateCode.addEventListener('click', function(event) {
 		const code = codeArea.value;
 		const request = new XMLHttpRequest();
@@ -15,8 +21,112 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		request.onreadystatechange = function() {
 			if(request.readyState !== 4) return;
 
+			if(request.status != 200) {
+			    alert(request.response);
+			    return;
+            }
+            localStorage['code-translators'] = code;
+
 			console.log(`${request.status} : ${request.statusText}`);
 			console.log(request.response);
+			showTables(JSON.parse(request.response));
+			buildRPNButton.removeAttribute('disabled');
 		}
 	});
-})
+    buildRPNButton.addEventListener('click', function(event) {
+        const request = new XMLHttpRequest();
+        request.open('POST', '/rpn', true);
+        request.setRequestHeader("Content-type", "application/json");
+
+        request.send();
+        request.onreadystatechange = function() {
+            if(request.readyState !== 4) return;
+
+            console.log(`${request.status} : ${request.statusText}`);
+            console.log(request.response);
+            // showTables(JSON.parse(request.response));
+            // buildRPNButton.removeAttribute('disabled');
+            showRPN(JSON.parse(request.response));
+        }
+    })
+});
+
+const lexTableTemplate = Handlebars.compile(`
+<h4>{{title}}</h4>
+<table class="table table-striped">
+<thead>
+    <tr><td>Lexeme</td><td>Line</td>{{#if type}}<td>Type</td>{{/if}}<td>Index</td></tr>
+</thead>
+<tbody>
+    {{#each table}}
+    <tr><td>{{lexeme}}</td><td>{{line}}</td>{{#if ../type}}<td>{{type}}</td>{{/if}}<td>{{index}}</td></tr>
+    {{/each}}
+</tbody>
+`);
+
+function showTables(tables) {
+    lexTables = tables;
+    const lexemeTableDiv = document.querySelector('.js-lex-table');
+    tables.lexemesTable.forEach(l => l.lexeme = l.lexeme? l.lexeme.replace('\n','¶'):null);
+    lexemeTableDiv.innerHTML = lexTableTemplate({table : tables.lexemesTable, type : true ,title:"Lexemes"});
+    const idTable = document.querySelector('.js-id-table');
+    idTable.innerHTML = lexTableTemplate({table : tables.idTable, title : "ID"});
+    const constTable = document.querySelector('.js-const-table');
+    constTable.innerHTML = lexTableTemplate({table : tables.constTable, title : "CONST"});
+
+}
+
+const labelsTableTemplate = Handlebars.compile(`
+<h3>Label</h3>
+<table class="table table-striped">
+<thead>
+    <tr><td>Lable</td><td>Link</td></tr>
+</thead>
+<tbody>
+{{#each labels}}
+    <tr><td>{{@key}}</td><td>{{this}}</td></tr>
+{{/each}}
+</tbody>
+</table>
+`);
+
+const rpnTemplate = Handlebars.compile(`
+<h3>RPN</h3>
+<table class="table table-striped">
+<thead>
+    <tr><td>#</td><td></td></tr>
+</thead>
+<tbody>
+{{#each rpn}}
+    <tr><td>{{@index}}</td><td>
+        {{#if lexeme}}{{lexeme}}{{/if}}
+        {{#if isLabel}}{{label}}{{/if}}
+        {{#if isJ}}{{text}}{{/if}}
+        </td></tr>
+{{/each}}
+</tbody>
+</table>
+`)
+
+const historyTemplate = Handlebars.compile(`
+<h3>History</h3>
+<table class="table table-striped">
+<thead>
+    <tr><td>input</td><td>stack</td><td>output</td></tr>
+</thead>
+<tbody>
+{{#each history}}
+      <tr><td>{{lexeme}}</td><td>{{stack}}</td><td>{{output}}</td></tr>
+{{/each}}
+</tbody>
+</table>
+`)
+function showRPN(data) {
+    const labelsContainer = document.querySelector('.js-poliz-labels');
+    labelsContainer.innerHTML = labelsTableTemplate({labels : data.labelsTable});
+    const polizListContainer = document.querySelector('.js-poliz-list');
+    polizListContainer.innerHTML = rpnTemplate({rpn : data.rpn});
+
+    const historyContainer = document.querySelector('.js-poliz-history');
+    historyContainer.innerHTML = historyTemplate({history : data.history});
+}
